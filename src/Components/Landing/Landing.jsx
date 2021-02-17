9b20ad11752d20d2422e9f6997fdac8062819839
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-17 19:23:24 +0530

    calling component in landing

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { UGCoursesDetails } from "../LeftNav/A-HomeAllCourses/UGCoursesDetails";
 import { Navbars } from "../Navbar/Navbars";
 export const Landing = () => {
   return (
     <div>
       <Navbars />
+      <UGCoursesDetails />
     </div>
   );
 };
