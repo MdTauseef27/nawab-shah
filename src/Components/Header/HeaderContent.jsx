c513e2299e0aa0efaa5e55ac15fcdfd50767e53b
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-25 11:12:40 +0530

    created header component

---
+++ src/Components/Header/HeaderContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+export const HeaderContent = () => {
+  return (
+    <div>
+      <Card>
+        <h1>Header Content Comes here</h1>
+      </Card>
+    </div>
+  );
+};
