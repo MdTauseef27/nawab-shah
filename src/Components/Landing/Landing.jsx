56005e1508d469519bce22dd013140304837d0a1
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-25 11:15:06 +0530

    calling header component into the landing page

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { UGCoursesDetails } from "../LeftNav/A-HomeAllCourses/UGCoursesDetails";
 import { Navbars } from "../Navbar/Navbars";
+import { HeaderContent } from "../Header/HeaderContent";
 export const Landing = () => {
   return (
     <div>
+      <HeaderContent />
       <Navbars />
-      <UGCoursesDetails />
+      {/* <UGCoursesDetails /> */}
     </div>
   );
 };
