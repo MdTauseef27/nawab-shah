0712e2c72a9f4558fc1ca6f0a4ef642db4b386f2
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-03-01 20:22:58 +0530

    added image into carousel

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import imgLogo1 from "../Images/HomeCarousels1.png";
+import imgLogo2 from "../Images/HomeCarousels2.png";
+// import imgLogo3 from "../Images/HomeCarousels3.png";
+
 export const Homepage = () => {
   return (
     <div>
@@ -9,7 +12,10 @@ export const Homepage = () => {
         <Carousel.Item>
           <img src={imgLogo1} />
         </Carousel.Item>
-        <Carousel.Item>{/* <img src={imgLogo2} /> */}</Carousel.Item>
+        <Carousel.Item>
+          {" "}
+          <img src={imgLogo2} />{" "}
+        </Carousel.Item>
         <Carousel.Item>{/* <img src={imgLogo3} /> */}</Carousel.Item>
       </Carousel>
     </div>
