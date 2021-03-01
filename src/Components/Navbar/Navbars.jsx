d266843074feba7badb2281c739b560a96df6786
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-03-01 19:55:49 +0530

    changes in the navbar component

--- src/Components/Navbar/Navbars.jsx
+++ src/Components/Navbar/Navbars.jsx
@@ -2,28 +2,71 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../Navbar/Navbars.css";
 
 import { UGCoursesDetails } from "../LeftNav/A-HomeAllCourses/UGCoursesDetails";
 import { MYHome } from "../MYHome";
 export const Navbars = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">HOME</Nav.Link>
-          <Nav.Link href="#features">ABOUT US </Nav.Link>
-          <Nav.Link href="/CSEBTECH">COURSES</Nav.Link>
-          <Nav.Link href="#department">DEPARTMENT</Nav.Link>
-          <Nav.Link href="#pricing">ADMISSIONS</Nav.Link>
-          <Nav.Link href="#pricing">PLACEMENTS</Nav.Link>
-          <Nav.Link href="#pricing">FEATURES</Nav.Link>
-          <Nav.Link href="#pricing">EVENTS</Nav.Link>
-          <Nav.Link href="#pricing">GALLERY</Nav.Link>
-          <Nav.Link href="#pricing">EXAMINATION CELL</Nav.Link>
-          <Nav.Link href="#pricing">R & D CELL</Nav.Link>
-          <Nav.Link href="#pricing">CONTACT US</Nav.Link>
+      <h6
+        style={{
+          content: "",
+          height: "3px",
+          width: "1260px",
+          background: "#EE7C7C",
+          position: "absolute",
+        }}
+      ></h6>
+      <Navbar>
+        <Nav>
+          <Nav.Link className="navbarlink" href="/">
+            HOME
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#features">
+            ABOUT US
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="/CSEBTECH">
+            COURSES
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#department">
+            DEPARTMENT
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#pricing">
+            ADMISSIONS
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#pricing">
+            PLACEMENTS
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#pricing">
+            FEATURES
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#pricing">
+            EVENTS
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#pricing">
+            GALLERY
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#pricing">
+            EXAMINATION CELL
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#pricing">
+            R & D CELL
+          </Nav.Link>
+          <Nav.Link className="navbarlink" href="#pricing">
+            CONTACT US
+          </Nav.Link>
         </Nav>
       </Navbar>
+      <h6
+        style={{
+          content: "",
+          height: "3px",
+          width: "1260px",
+          background: "#EE7C7C",
+          position: "absolute",
+        }}
+      ></h6>
       <Router>
         <Switch>
           <Route path="/CSEBTECH">
