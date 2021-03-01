98b17618c95252fdc2570ba179274317e0ee8c0b
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-03-01 19:56:34 +0530

    created navbarmain component

---
+++ src/Components/Navbar/NavbarMain.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+export const NavbarMain = () => {
+  return (
+    <div>
+      <Navbar className="navbarmain">
+        <Nav>
+          <Nav.Link className="navbarmainlink" href="/">
+            AICTE Approvals
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#features">
+            OU & JNTUH Approvals
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="/CSEBTECH">
+            SBTET Approvals
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#department">
+            Grievence Redressel cell
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#pricing">
+            NSAKCET News Blog
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#pricing">
+            Campus Placements
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#pricing">
+            Student Corner
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#pricing">
+            Alumini
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#pricing">
+            NAAC
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#pricing">
+            IQAC
+          </Nav.Link>
+          {/* <Nav.Link className="navbarmainlink" href="#pricing">
+            Online Fee Payment
+          </Nav.Link>
+          <Nav.Link className="navbarmainlink" href="#pricing">
+            EAMCET & JEE Mock Test
+          </Nav.Link> */}
+        </Nav>
+      </Navbar>
+    </div>
+  );
+};
