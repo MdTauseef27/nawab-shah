7e5666fed02ae5c32547948910775a0effcf1b22
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-03-02 15:56:44 +0530

    added two cards on homepage

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Carousel, Row, Col } from "react-bootstrap";
+import { Carousel, Row, Col, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import imgLogo1 from "../Images/HomeCarousels1.png";
 import imgLogo2 from "../Images/HomeCarousels2.png";
 import imgLogo3 from "../Images/HomeCarousels3.png";
+import cardLogo1 from "../Images/HomeCard1.png";
 import "../HomePage/Homepage.css";
 export const Homepage = () => {
   return (
@@ -26,6 +27,38 @@ export const Homepage = () => {
           >
             NSAKCET
           </h6>
+          <Row>
+            <Card
+              style={{
+                height: "200px",
+                width: "275px",
+                marginTop: "30px",
+                marginBottom: "40px",
+
+                borderRadius: "20px",
+                marginLeft: "22px",
+                overflow: "hidden",
+                borderRadius: "10px",
+              }}
+            >
+              <Card.Img src={cardLogo1}></Card.Img>
+            </Card>
+            <Card
+              style={{
+                height: "200px",
+                width: "275px",
+                marginTop: "30px",
+                marginBottom: "40px",
+
+                borderRadius: "20px",
+                marginLeft: "22px",
+                overflow: "hidden",
+                borderRadius: "10px",
+              }}
+            >
+              <Card.Img src={cardLogo1}></Card.Img>
+            </Card>
+          </Row>
         </Col>
         <Col lg="4">
           <h6
