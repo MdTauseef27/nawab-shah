6033ec6e293e9bf56b30b054ddae909f2edd29c6
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-03-03 12:19:56 +0530

    added some css

--- src/Components/HomePage/Homepage.jsx
+++ src/Components/HomePage/Homepage.jsx
@@ -23,104 +23,112 @@ export const Homepage = () => {
         </Carousel.Item>
       </Carousel>
       <Row>
-        <Col lg="9">
+        {/* <Col lg="9"> */}
+
+        <Col lg="3">
           <h6 className="HomeHeader6">NSAKCET</h6>
-          <Row>
-            <Card className="HomeCards">
-              <Card.Img src={cardLogo1}></Card.Img>
-            </Card>
-            <Card className="HomeCards">
-              <Card.Img src={cardLogo2}></Card.Img>
-            </Card>
-            <Card className="HomeCards">
-              <Card.Img src={cardLogo1}></Card.Img>
-            </Card>
-          </Row>
-          <Row>
-            <Col lg="3">
-              <Nav.Link style={{ float: "left" }}>NSAKCET</Nav.Link>
-              <br></br>
-              <p
-                style={{
-                  textAlign: "justify",
-                  marginLeft: "15px",
-                  width: "275px",
-                  lineHeight: "20px",
-                  marginTop: "20px",
-                }}
-              >
-                The synonym for high quality education. We have earned that name
-                for the exceptional service provided in all these years.
-              </p>
-              <Button style={{ float: "left" }}>Read More</Button>
-            </Col>
-            <Col lg="3">
-              <Nav.Link style={{ float: "left", marginLeft: "62px" }}>
-                LABORATORIES
-              </Nav.Link>
-              <br></br>
-              <p
-                style={{
-                  textAlign: "justify",
-                  marginLeft: "75px",
-                  width: "275px",
-                  lineHeight: "20px",
-                  marginTop: "20px",
-                }}
-              >
-                Our Laboratories for all the departments are well-equipped and
-                state of the art and in sync with the JNTU syllabus taught
-              </p>
-              <Button style={{ float: "left", marginLeft: "75px" }}>
-                Read More
-              </Button>
-            </Col>
-            <Col>
-              <Nav.Link style={{ float: "left", marginLeft: "125px" }}>
-                EMINENT PROFESSORS
-              </Nav.Link>
-              <br></br>
-              <p
-                style={{
-                  textAlign: "justify",
-                  marginLeft: "140px",
-                  width: "275px",
-                  lineHeight: "20px",
-                  marginTop: "20px",
-                }}
-              >
-                Eminent Professors and Research Team at NSAKCET.
-              </p>
-              <Button
-                style={{
-                  float: "left",
-                  marginTop: "30px",
-                  marginLeft: "140px",
-                }}
-              >
-                Read More
-              </Button>
-            </Col>
-          </Row>
+          <Card className="HomeCards">
+            <Card.Img src={cardLogo1}></Card.Img>
+          </Card>
+          <Nav.Link style={{ float: "left", marginLeft: "5px" }}>
+            NSAKCET
+          </Nav.Link>
+          <br></br>
+          <p
+            style={{
+              textAlign: "justify",
+              marginLeft: "20px",
+              width: "275px",
+              lineHeight: "20px",
+              marginTop: "20px",
+            }}
+          >
+            The synonym for high quality education. We have earned that name for
+            the exceptional service provided in all these years.
+          </p>
+          <Button style={{ float: "left", marginLeft: "20px" }}>
+            Read More
+          </Button>
         </Col>
+        <Col lg="3">
+          <Card className="HomeCards" style={{ marginTop: "48px" }}>
+            <Card.Img src={cardLogo2}></Card.Img>
+          </Card>
+          <Nav.Link style={{ float: "left", marginLeft: "5px" }}>
+            LABORATORIES
+          </Nav.Link>
+          <br></br>
+          <p
+            style={{
+              textAlign: "justify",
+              marginLeft: "20px",
+              width: "275px",
+              lineHeight: "20px",
+              marginTop: "20px",
+            }}
+          >
+            Our Laboratories for all the departments are well-equipped and state
+            of the art and in sync with the JNTU syllabus taught
+          </p>
+          <Button style={{ float: "left", marginLeft: "20px" }}>
+            Read More
+          </Button>
+        </Col>
+        <Col lg="3">
+          <Card className="HomeCards" style={{ marginTop: "48px" }}>
+            <Card.Img src={cardLogo1}></Card.Img>
+          </Card>
+          <Nav.Link style={{ float: "left", marginLeft: "5px" }}>
+            EMINENT PROFESSORS
+          </Nav.Link>
+          <br></br>
+          <p
+            style={{
+              textAlign: "justify",
+              marginLeft: "20px",
+              width: "275px",
+              lineHeight: "20px",
+              marginTop: "20px",
+            }}
+          >
+            Eminent Professors and Research Team at NSAKCET.
+          </p>
+          <Button
+            style={{
+              float: "left",
+              marginTop: "30px",
+              marginLeft: "20px",
+            }}
+          >
+            Read More
+          </Button>
+        </Col>
+
         <Col lg="3">
           <h6 className="HomeHeader6">LATEST NEWS</h6>
           <Card
             style={{
-              width: "18rem",
-              height: "25rem",
+              width: "16rem",
+              height: "18rem",
               marginTop: "30px",
               backgroundColor: "#EE7C7C",
             }}
           >
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
+              <p>https://payments.billdesk.com/bdcollect/pay?p1=478&amp;p2=1</p>
               <hr></hr>
+              <p>Nsakcet video https://youtu.be/2gEEAHKGfAQ</p>
+              <hr></hr>
+              <p>https://youtu.be/mLWRTBgqHuM</p>
+            </Card.Body>
+          </Card>
+          <Card
+            style={{
+              width: "16rem",
+              height: "7rem",
+            }}
+          >
+            <Card.Body>
               <Nav.Link>COURSES</Nav.Link>
               <Nav.Link>Study Material</Nav.Link>
             </Card.Body>
