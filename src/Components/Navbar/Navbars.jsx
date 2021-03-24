c9757ef26925e53c14d343ecd94c56010cc40de8
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-03-24 18:31:10 +0530

    calling the UGCourse and CiscoNW component

--- src/Components/Navbar/Navbars.jsx
+++ src/Components/Navbar/Navbars.jsx
@@ -719,6 +719,9 @@ export const Navbars = () => {
           </Route>
           <Route path="/CISCONetworkingAcademy">
             <Row>
+              <Col lg="3">
+                <UGCoursesDetails />
+              </Col>
               <Col lg="9">
                 <CiscoNetworking />
               </Col>
