3fcbe28cf5ce5e13d87258afdc10d430d31fa5b5
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-04-05 16:21:47 +0530

    deleted the imported component

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { UGCoursesDetails } from "../LeftNav/A-HomeAllCourses/UGCoursesDetails";
 import { Navbars } from "../Navbar/Navbars";
 import { HeaderContent } from "../Header/HeaderContent";
-import { Container } from "react-bootstrap";
 import { FooterContent } from "../Footer/FooterContent";
 import { Card } from "react-bootstrap";
 import { NavbarMain } from "../Navbar/NavbarMain";
