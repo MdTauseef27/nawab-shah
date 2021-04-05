54e13e3f076bbe2be11aee9fdf425897bd7ae876
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-04-05 16:10:49 +0530

    added the responsive navbar into navbar.jsx component

--- src/Components/Navbar/Navbars.jsx
+++ src/Components/Navbar/Navbars.jsx
@@ -67,46 +67,53 @@ export const Navbars = () => {
           position: "absolute",
         }}
       ></h6>
-      <Navbar>
-        <Nav>
-          <Nav.Link className="navbarlink" href="/">
-            HOME
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#features">
-            ABOUT US
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="/CSEBTECH">
-            COURSES
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#department">
-            DEPARTMENT
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#pricing">
-            ADMISSIONS
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#pricing">
-            PLACEMENTS
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#pricing">
-            FEATURES
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#pricing">
-            EVENTS
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#pricing">
-            GALLERY
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#pricing">
-            EXAMINATION CELL
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#pricing">
-            R & D CELL
-          </Nav.Link>
-          <Nav.Link className="navbarlink" href="#pricing">
-            CONTACT US
-          </Nav.Link>
-        </Nav>
+      <Navbar collapseOnSelect>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          className="NavCollapsed"
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link className="navbarlink" href="/">
+              HOME
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#features">
+              ABOUT US
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="/CSEBTECH">
+              COURSES
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#department">
+              DEPARTMENT
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#pricing">
+              ADMISSIONS
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#pricing">
+              PLACEMENTS
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#pricing">
+              FEATURES
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#pricing">
+              EVENTS
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#pricing">
+              GALLERY
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#pricing">
+              EXAMINATION CELL
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#pricing">
+              R & D CELL
+            </Nav.Link>
+            <Nav.Link className="navbarlink" href="#pricing">
+              CONTACT US
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
+
       <h6
         style={{
           content: "",
