675928b87327f1537a02dc3a85c0f82160f28f4c
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-04-05 16:22:12 +0530

    changes in the landing component

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { UGCoursesDetails } from "../LeftNav/A-HomeAllCourses/UGCoursesDetails";
 import { Navbars } from "../Navbar/Navbars";
 import { HeaderContent } from "../Header/HeaderContent";
 import { FooterContent } from "../Footer/FooterContent";
